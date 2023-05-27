b4dfaf707abf8857f8f960b8f583089d8cedbb42
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-27 15:41:25 +0400

    adding routes.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route} from 'react-router-dom'
+import Categories from './components/categories-cont/Categories';
+import Home from './routes/home/Home';
+import Navigation from './routes/navigation/Navigation';
+import SignIn from './routes/sign-in/signIn';
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home/>} />
+        <Route path='sign-in' element={<SignIn/>} />
+        </Route>
+
+    </Routes>
   );
 }
 
