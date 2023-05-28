8b4cf1d5a9b4e986200262765e4fb12cd1ba0935
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-28 12:29:28 +0400

    changed the name of route path.

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import {Routes,Route} from 'react-router-dom'
 import Categories from './components/categories-cont/Categories';
 import Home from './routes/home/Home';
 import Navigation from './routes/navigation/Navigation';
-import SignIn from './routes/sign-in/signIn';
+import Autentecition from './routes/sign-in/Autentecition';
 
 function App() {
  
@@ -10,7 +10,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigation/>}>
         <Route index element={<Home/>} />
-        <Route path='sign-in' element={<SignIn/>} />
+        <Route path='auth' element={<Autentecition/>} />
         </Route>
 
     </Routes>
