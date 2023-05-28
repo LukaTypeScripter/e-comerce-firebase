7e3e6ff3b4949f0d419efa814541e86323777ef3
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-28 19:46:25 +0400

    added context proviuder.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {UserProvider} from './contexts/user.contexts'
 import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <UserProvider>
     <App />
+    </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
