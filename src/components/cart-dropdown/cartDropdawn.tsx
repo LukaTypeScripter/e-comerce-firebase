94324ae6a38c532be8c48ba6e384d8c336a36de1
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-29 13:23:33 +0400

    map cart items to display in dropdown

--- src/components/cart-dropdown/cartDropdawn.tsx
+++ src/components/cart-dropdown/cartDropdawn.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
-import './cartDropdawn.scss' 
-import Button from '../button/Button'
 import { useContext } from 'react'
 import { DropdownContext } from '../../DropDown-context/Dropdown.context'
+import './cartDropdawn.scss' 
+import Button from '../button/Button'
+import CartItem from '../cart-item/CartItem'
 function CartDropdawn() {
-    const {isOpen,setIsOpen} = useContext(DropdownContext)
+    const {cartItems} = useContext(DropdownContext)
   return (
     <div className='cart-dropdown-container'>
-        <div className='cart-items'/>
+        <div className='cart-items'>
+        {cartItems.map((item) => {
+            return <CartItem key={item.id} cartItems={item} />
+        })}
+        </div>
         <Button >Go to Check out</Button>
         
     </div>
   )
-}
+} 
 
 export default CartDropdawn
