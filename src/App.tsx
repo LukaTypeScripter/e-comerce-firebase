3e7cf204cf974765007765939c360e8b4a3406f5
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-29 18:43:31 +0400

    checkout route.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import {Routes,Route} from 'react-router-dom'
-import Categories from './components/categories-cont/Categories';
+
 import Home from './routes/home/Home';
 import Navigation from './routes/navigation/Navigation';
 import Autentecition from './routes/sign-in/Autentecition';
 import Shop from './routes/shop/Shop';
+import CheckOut from './routes/checkout/CheckOut';
 
 function App() {
  
@@ -13,6 +14,7 @@ function App() {
         <Route index element={<Home/>} />
         <Route path='shop' element={<Shop/>} />
         <Route path='auth' element={<Autentecition/>} />
+        <Route path='checkout' element={<CheckOut/>} />
         </Route>
 
     </Routes>
