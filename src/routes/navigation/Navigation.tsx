6c13e1bf646cd25b92592faf459adf4395d2663f
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-29 11:47:45 +0400

    added new cart svg + adde new routes

--- src/routes/navigation/Navigation.tsx
+++ src/routes/navigation/Navigation.tsx
@@ -4,14 +4,21 @@ import {ReactComponent as CrownLogo} from '../../assets/007 crown.svg'
 import './navigation.scss'
 import { UserContext } from '../../contexts/user.contexts'
 import { signOutUser } from '../../utlis/firbase/firebase'
+import CartIcon from '../../components/cart-icon/CartIcon'
+import cartDropdawn from '../../components/cart-dropdown/cartDropdawn'
+import CartDropdawn from '../../components/cart-dropdown/cartDropdawn'
+import { DropdownContext } from '../../DropDown-context/Dropdown.context'
 function Navigation() {
+  
   const {currentUser,setCurrentUser} = useContext(UserContext)
+  const {isOpen} = useContext(DropdownContext)
   const signOutHandler = async () => {
      await signOutUser();
     setCurrentUser(null)
     
   }
   console.log(currentUser)
+
   return (
     <>
         <div className='navigation'>
@@ -20,11 +27,15 @@ function Navigation() {
             </Link>
         
         <div className="nav-links-container">
-        <Link className='nav-link' to="/"></Link>
+        <Link className='nav-link' to="/shop">Shop</Link>
         {currentUser ? (<span className='nav-links' onClick={signOutHandler}>SIGN OUT</span>) : (<Link className='nav-link' to="/auth">Sign in</Link>)}
+          <CartIcon/>
+        </div>
+        </div>
+        {isOpen && (
+          <CartDropdawn/>
+        )}
         
-        </div>
-        </div>
         <Outlet/>
     </>
   )
