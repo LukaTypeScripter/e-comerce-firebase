f32c34825a06b1f961adb50d11eaf6c59d7cd71b
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-29 13:25:31 +0400

    added find cloathes by id and add 1 to qty

--- src/DropDown-context/Dropdown.context.tsx
+++ src/DropDown-context/Dropdown.context.tsx
@@ -1,19 +1,48 @@
 import { createContext,useState } from "react";
-
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    imageUrl: string;
+    quantity?:number
+}
 interface ProductContextValue {
     isOpen: boolean;
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>
+    cartItems:Product[];
+    addItemToCart: (productToAdd: Product) => void;
 }
-
+const addCartItem = (cartItems: Product[], productToAdd: Product) => {
+    const existingCartItem = cartItems.find((item) => item.id === productToAdd.id);
+  
+    if (existingCartItem) {
+      return cartItems.map((cartItem) =>
+        cartItem.id === productToAdd.id
+          ? { ...cartItem, quantity: (cartItem.quantity! + 1) }
+          : cartItem
+      );
+    }
+  
+  
+  
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+  };
+  
 export const DropdownContext = createContext<ProductContextValue>({
     isOpen:false,
-    setIsOpen:()=>{}
+    setIsOpen:()=>{},
+    cartItems:[],
+    addItemToCart: () => {},
 })
 
 export const DropdownProvider = ({children}: { children: React.ReactNode }) => {
     const [isOpen,setIsOpen] = useState(false)
+    const [cartItems,setCartitems] = useState<Product[]>([])
+    const addItemToCart = (productToAdd:Product) => {
+        setCartitems(addCartItem(cartItems, productToAdd))
+    }
     return (
-        <DropdownContext.Provider value={{isOpen,setIsOpen}}>
+        <DropdownContext.Provider value={{isOpen,setIsOpen,addItemToCart,cartItems}}>
             {children}
         </DropdownContext.Provider>
     )
