54a934bdfb03865ea9da4c5330fdeeecc9cb7a0e
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-29 11:45:21 +0400

    connected contexts

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {UserProvider} from './contexts/user.contexts'
 import { BrowserRouter } from 'react-router-dom';
+import { ProductsProvider } from './shop-context/shop.contexts';
+import { DropdownProvider } from './DropDown-context/Dropdown.context';
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    
     <UserProvider>
+    <ProductsProvider>
+      <DropdownProvider>
     <App />
+    </DropdownProvider>
+    </ProductsProvider>
     </UserProvider>
+    
     </BrowserRouter>
   </React.StrictMode>
 );
