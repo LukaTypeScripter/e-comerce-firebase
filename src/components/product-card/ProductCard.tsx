e14ed8a977dbb7263018b2db6d40bdb6cdea972a
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-29 13:24:53 +0400

    using addItemCart func to find by id and add 1 qty

--- src/components/product-card/ProductCard.tsx
+++ src/components/product-card/ProductCard.tsx
@@ -1,24 +1,28 @@
 import React from 'react'
 import './ProductCard.scss'
 import Button from '../button/Button'
-
-interface Product {
-    id: number;
-    name: string;
-    price: number;
-    imageUrl: string;
-   
-}
-function ProductCard({id,name,price,imageUrl}:Product) {
-    
-  return (
+import { useContext } from 'react'
+import { DropdownContext } from '../../DropDown-context/Dropdown.context'
+interface ProductProps {
+    product: {
+      id: number;
+      name: string;
+      price: number;
+      imageUrl: string;
+    };
+  }
+function ProductCard({product}:ProductProps) {
+    const { id, name, price, imageUrl } = product;
+    const {addItemToCart} = useContext(DropdownContext)
+    const addProductToCart = () => addItemToCart(product)
+    return (
     <div className='product-card-container' key={id}>
         <img src={imageUrl} alt={name} />
         <div className='footer'>
             <span className='name'>{name}</span>
             <span className='price'>{price}</span>
         </div>
-        <Button buttonType='inverted'>Add to card!</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>Add to card!</Button>
     </div>
   )
 }
