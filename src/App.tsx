65a301676eb057e10e9fae4fb8138586a107a29a
luka shinjiakshvili <114741110+lukapain@users.noreply.github.com>
2023-05-30 22:44:58 +0400

    chenged route to match cloating title.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Navigation/>}>
         <Route index element={<Home/>} />
-        <Route path='shop' element={<Shop/>} />
+        <Route path='shop/*' element={<Shop/>} />
         <Route path='auth' element={<Autentecition/>} />
         <Route path='checkout' element={<CheckOut/>} />
         </Route>
